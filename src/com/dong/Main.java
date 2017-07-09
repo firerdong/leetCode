4f3df0cde4d10acc9724067948418c62d703d27b
firerdong <firerdong@qq.com>
2017-07-09 20:28:08 +0800

    69.Sqrt(x)

---
+++ src/com/dong/Main.java
@@ -0,0 +1,14 @@
+package com.dong;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for(int i: new int[]{2,3,4,5,8,9,10,15,16,17}) {
+			System.out.println(Sqrt.mySqrt(i));
+		}
+	}
+
+}
---
+++ src/com/dong/Sqrt.java
@@ -0,0 +1,17 @@
+package com.dong;
+
+public class Sqrt {
+    public static int mySqrt(int x) {
+    	if(x<=1){
+    		return x;
+    	}
+    	long i=0;// should be set long
+    	for(i=1; i<=x/2; i++){
+    		if(i*i == x || 
+    				(i*i<x && x<(i+1)*(i+1))) {
+    			return (int)i;
+    		}
+    	}
+    	return (int)i;
+    }
+}
