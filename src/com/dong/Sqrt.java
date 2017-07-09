d42da1236b7c94a856cfcb8d26358a9f8d55813e
firerdong <firerdong@qq.com>
2017-07-09 20:58:10 +0800

    use binary search

--- src/com/dong/Sqrt.java
+++ src/com/dong/Sqrt.java
@@ -5,12 +5,19 @@ public class Sqrt {
     	if(x<=1){
     		return x;
     	}
-    	long i=0;// should be set long
-    	for(i=1; i<=x/2; i++){
-    		if(i*i<=x && x<(i+1)*(i+1)) {
-    			return (int)i;
-    		}
-    	}
-    	return (int)i;
+        long left = 1;
+        long right = x/2;
+        while(left<right){
+            long mid = (left+right)/2;
+            if(mid*mid<=x && x<(mid+1)*(mid+1)) {
+                return (int)mid;
+            } else if(mid*mid > x){
+                right = mid -1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+    	return (int)left;
     }
 }
