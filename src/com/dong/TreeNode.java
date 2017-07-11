5dd3a018725faa5cdeaf50693845ad919df16177
donghy <hongyu.dong@secneo.com>
2017-07-11 16:55:48 +0800

    111 minimun depth of binary tree

--- src/com/dong/TreeNode.java
+++ src/com/dong/TreeNode.java
@@ -249,4 +249,23 @@ public class TreeNode {
     	}
     	
     }
+    
+    public static int minDepth(TreeNode root) {
+        if(root == null) {
+        	return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        int ret=0;
+        if(left == 0 && right == 0) {
+        	return 1;
+        }
+        if(left == 0) {
+        	return right + 1;
+        }
+        if(right == 0) {
+        	return left + 1;
+        }
+        return Math.min(left, right) + 1;
+    }
 }
