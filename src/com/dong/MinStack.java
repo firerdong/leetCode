de7176700f12059d11b3b39d5d2b7d778fc37455
donghy <hongyu.dong@secneo.com>
2017-07-12 20:05:09 +0800

    temo

---
+++ src/com/dong/MinStack.java
@@ -0,0 +1,50 @@
+package com.dong;
+
+import java.util.Stack;
+
+public class MinStack {
+	
+	Stack<Integer> main = null;
+	Stack<Integer> min = null;
+	
+    public MinStack() {
+    	main = new Stack<Integer>(); 
+    	min = new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+        if(main.empty()){
+        	main.push(x);
+        	min.push(x);
+        }else{
+        	if(min.peek() > x) {
+        		main.push(x);
+        		min.push(x);
+        	}else{
+        		main.push(x);
+        		min.push(min.peek());
+        	}
+        }
+    }
+    
+    public void pop() {
+        if(!main.empty()) {
+        	main.pop();
+        	min.pop();
+        }
+    }
+    
+    public int top() {
+        if(!main.empty()) {
+        	return main.peek();
+        }
+        return 0;
+    }
+    
+    public int getMin() {
+        while(main.peek() != min.peek()) {
+        	main.pop();
+        	min.pop();
+        }
+    }
+}
